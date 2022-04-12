aaba36a2ec88350e8a9493ad6820e1fe9001fa20
Thiago Galindo <ecthiagoufpb@gmail.com>
2022-04-12 11:49:50 -0300

    1st commit and Setting up databinding.

---
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.udacity.shoestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.ActivityMainBinding
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var dataBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setContentView(R.layout.activity_main)
+        Timber.plant(Timber.DebugTree())
+    }
+}
